5fd84ac991ef9b9a3e19ac3381a6b776565db800
Alice <aliceawesome87@gmail.com>
2022-03-31 19:55:54 -0600

    Commit message

---
+++ transmitter/transmitter.ino
@@ -0,0 +1,29 @@
+//Include Libraries
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//create an RF24 object
+RF24 radio(9, 8);  // CE, CSN
+
+//address through which two modules communicate.
+const byte address[6] = "00001";
+
+void setup()
+{
+  radio.begin();
+  
+  //set the address
+  radio.openWritingPipe(address);
+  
+  //Set module as transmitter
+  radio.stopListening();
+}
+void loop()
+{
+  //Send message to receiver
+  int val = 5;
+  radio.write(&val, sizeof(val));
+
+  delay(1000);
+}
